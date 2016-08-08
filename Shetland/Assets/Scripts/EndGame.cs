2c97cfb8f24515cf877472a7e01eba9af32e9672
kdizzle87 <kcreanor@hotmail.com>
2016-08-08 21:52:42 +0100

    End Canvas initiated, operational script added

---
+++ Shetland/Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+	public int _obolReq;
+	public Canvas _harbourCanvas;
+
+	void Start(){
+		_harbourCanvas = GameObject.Find("HarbourCanvas").GetComponent<Canvas>();
+		_harbourCanvas.enabled = false;
+	}
+
+	public void OpenCanvas(){
+		_harbourCanvas.enabled = true;
+	}
+
+}
