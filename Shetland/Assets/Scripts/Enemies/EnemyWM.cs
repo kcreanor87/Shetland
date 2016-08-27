8c3bbb58786cee648e92c5be182bcd50cad266a8
kdizzle87 <kcreanor@hotmail.com>
2016-08-27 22:03:42 +0100

    Initial enemy sight radius configured

---
+++ Shetland/Assets/Scripts/Enemies/EnemyWM.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWM : MonoBehaviour {
+
+	public NavMeshAgent _agent;
+	public PlayerControls_WM _player;
+	public bool _spotted;
+
+	void Start(){
+		_agent = gameObject.GetComponent<NavMeshAgent>();
+		_player = GameObject.Find("Player").GetComponent<PlayerControls_WM>();
+
+	}
+
+	public void ChasePlayer(){
+		print("Player Spotted");
+		_spotted = true;
+		_agent.enabled = true;
+		StartCoroutine(ChaseLoop(0.5f));
+	}
+
+	public IEnumerator ChaseLoop(float looptime){
+		while (_spotted){
+			_agent.SetDestination(_player.transform.position);
+			yield return new WaitForSeconds(looptime);
+		}		
+	}
+
+	public void AtPlayer(){
+		_spotted = false;
+		_agent.SetDestination(transform.position);
+		_player._agent.SetDestination(_player.transform.position);
+		_player.enabled = false;
+		print("Combat!");
+	}
+}
---
+++ Shetland/Assets/Scripts/Enemies/PlayerSpotted.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerSpotted : MonoBehaviour {
+
+	public PlayerSpotted _thisScript;
+	public EnemyWM _parentScript;
+
+	void Start(){
+		_thisScript = gameObject.GetComponent<PlayerSpotted>();
+		_parentScript = transform.GetComponentInParent<EnemyWM>();
+	}
+
+	void OnWillRenderObject(){
+		if (Camera.current.name == "Main Camera"){
+			Seen();
+		}
+	}
+
+	void Seen(){
+		_parentScript.ChasePlayer();
+		_thisScript.enabled = false;
+	}
+}
